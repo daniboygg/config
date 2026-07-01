26d363a65554e194f067ea683fa4c8fb360bf78c
daniboygg <dani@daniboygg.dev>
2026-07-01 16:32:57 +0200

    Add sequence shortcuts to move windows between screens

--- dotfiles/.hammerspoon/utils.lua
+++ dotfiles/.hammerspoon/utils.lua
@@ -42,4 +42,62 @@ function M.cycleKeyboardLayout()
     end
 end
 
+function M.sequenceHotkey(mods, key)
+    local timeoutSeconds = 2
+    local modal = hs.hotkey.modal.new(mods, key)
+    local sequence = {}
+    local timer = nil
+
+    local function stopTimer()
+        if timer then
+            timer:stop()
+            timer = nil
+        end
+    end
+
+    function sequence.exit()
+        stopTimer()
+        modal:exit()
+        return sequence
+    end
+
+    function sequence.add(secondKey, action, secondMods)
+        modal:bind(secondMods or {}, secondKey, function()
+            action()
+            sequence.exit()
+        end)
+        return sequence
+    end
+
+    function sequence.cancel(secondKey, secondMods)
+        modal:bind(secondMods or {}, secondKey, function()
+            sequence.exit()
+        end)
+        return sequence
+    end
+
+    function modal:entered()
+        stopTimer()
+
+        timer = hs.timer.doAfter(timeoutSeconds, function()
+            sequence.exit()
+        end)
+    end
+
+    function modal:exited()
+        stopTimer()
+    end
+
+    sequence.cancel("escape")
+
+    return sequence
+end
+
+function M.rectangleAction(name)
+    return function()
+        hs.task.new("/usr/bin/open", nil, {"-g", "rectangle://execute-action?name=" .. name}):start()
+    end
+end
+
 return M
+
