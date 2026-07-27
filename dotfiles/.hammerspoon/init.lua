97a8a770855a751a915e9cc1b85f84011762d0b3
daniboygg <dani@daniboygg.dev>
2026-07-27 17:43:42 +0200

    Add shortcuts for half-screen

--- dotfiles/.hammerspoon/init.lua
+++ dotfiles/.hammerspoon/init.lua
@@ -17,6 +17,9 @@ hs.hotkey.bind(hyper, "i", utils.cycleKeyboardLayout)
 local windowSequenceHotkey = utils.sequenceHotkey(hyper, "p")
 windowSequenceHotkey.add("h", utils.rectangleAction("previous-display"))
 windowSequenceHotkey.add("l", utils.rectangleAction("next-display"))
+-- make window half of the screen left/right
+windowSequenceHotkey.add("h", utils.rectangleAction("left-half"), {"shift"})
+windowSequenceHotkey.add("l", utils.rectangleAction("right-half"), {"shift"})
 -- Window [p]osition [m]aximize
 windowSequenceHotkey.add("m", utils.rectangleAction("maximize"))
 -- Window [p]osition [s]wap frontmost windows
